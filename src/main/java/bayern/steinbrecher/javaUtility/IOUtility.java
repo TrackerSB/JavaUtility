5ea00c0a3bf5643acc8a2eda5aac676b842ce512
Stefan Huber <stefan.huber.niedling@outlook.com>
2020-11-29 16:52:49 +0100

    IOUtility: Add JSch compatible read process

--- src/main/java/bayern/steinbrecher/javaUtility/IOUtility.java
+++ src/main/java/bayern/steinbrecher/javaUtility/IOUtility.java
@@ -1,6 +1,8 @@
 package bayern.steinbrecher.javaUtility;
 
+import bayern.steinbrecher.jsch.ChannelExec;
 import javafx.concurrent.Task;
+import javafx.util.Pair;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -24,6 +26,11 @@ public final class IOUtility {
         throw new UnsupportedOperationException("The construction of instances is prohibited");
     }
 
+    /**
+     * WARNING This method is neither geared to read continues output of interactive shells nor remote shells nor very
+     * large outputs. In these cases consider using {@link #readChannelContinuously(ChannelExec)}. However, this method is
+     * way faster in reading than {@link #readChannelContinuously(ChannelExec)}.
+     */
     public static String readAll(InputStream inputStream, Charset charset) throws IOException {
         StringBuilder output = new StringBuilder();
         try (ReadableByteChannel rbc = Channels.newChannel(inputStream)) {
@@ -73,5 +80,48 @@ public final class IOUtility {
             }
         }
         return output.toString();
+    }
+
+    /**
+     * Based on https://stackoverflow.com/a/47554723/4863098
+     */
+    public static Pair<String, String> readChannelContinuously(ChannelExec channel) throws IOException {
+        StringBuilder outputBuffer = new StringBuilder();
+        StringBuilder errorBuffer = new StringBuilder();
+        InputStream inStream = channel.getInputStream();
+        InputStream errStream = channel.getErrStream();
+        byte[] tmp = new byte[DEFAULT_BUFFER_SIZE];
+        while (true) {
+            while (inStream.available() > 0) {
+                int i = inStream.read(tmp, 0, DEFAULT_BUFFER_SIZE);
+                if (i < 0) {
+                    break;
+                }
+                outputBuffer.append(new String(tmp, 0, i));
+            }
+            while (errStream.available() > 0) {
+                int i = errStream.read(tmp, 0, DEFAULT_BUFFER_SIZE);
+                if (i < 0) {
+                    break;
+                }
+                errorBuffer.append(new String(tmp, 0, i));
+            }
+            if (channel.isClosed()) {
+                if ((inStream.available() > 0) || (errStream.available() > 0)) {
+                    continue;
+                }
+                int exitStatus = channel.getExitStatus();
+                if (exitStatus != 0) {
+                    LOGGER.log(Level.WARNING, "The channel finished with a non-zero exit state");
+                }
+                break;
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                LOGGER.log(Level.WARNING, "The delay before the next read iteration was interrupted.", ex);
+            }
+        }
+        return new Pair<>(outputBuffer.toString(), errorBuffer.toString());
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module bayern.steinbrecher.Utility {
     exports bayern.steinbrecher.javaUtility;
 
+    requires bayern.steinbrecher.jsch;
     requires java.logging;
     requires java.xml;
     requires javafx.base;
