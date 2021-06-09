0d2db1727d4a5cae271fbf127ad9944731b99f95
Stefan Huber <stefan.huber.niedling@outlook.com>
2021-06-09 16:50:15 +0200

    IOUtility: Avoid NPEs

--- src/main/java/bayern/steinbrecher/javaUtility/IOUtility.java
+++ src/main/java/bayern/steinbrecher/javaUtility/IOUtility.java
@@ -14,6 +14,7 @@ import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.charset.Charset;
 import java.nio.file.Path;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -155,7 +156,7 @@ public final class IOUtility {
                     if (!isFirstCell) {
                         writer.write(format.getSeparator());
                     }
-                    writer.write(cell);
+                    writer.write(Objects.requireNonNullElse(cell, ""));
                     isFirstCell = false;
                 }
                 writer.write('\n');
